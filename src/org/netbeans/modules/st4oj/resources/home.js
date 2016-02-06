017e0d5dda90cb111d2285753af35701649d0b4e
gwieleng <gwieleng@GWIELENG-NL.nl.oracle.com>
2016-02-06 11:06:49 +0100

    Templates for ojNavigationList.

--- src/org/netbeans/modules/st4oj/resources/home.js
+++ src/org/netbeans/modules/st4oj/resources/home.js
@@ -1,16 +1,19 @@
 /**
  * Home module
  */
-define(['ojs/ojcore' ,'knockout'
-   ], function(oj, ko) {
-   /**
-    * The view model for the main content view template
-    */
-            function homeContentViewModel() {
-                var self = this;
-                
-                self.something = ko.observable("\This paragraph uses content from its own 'home' ViewModel.");
-            }
+define(['ojs/ojcore', 'knockout'
+], function (oj, ko) {
+    /**
+     * The view model for the main content view template
+     */
+    function homeContentViewModel() {
+        var self = this;
+        self.firstName = ko.observable("Planet");
+        self.lastName = ko.observable("Earth");
+        self.fullName = ko.pureComputed(function () {
+            return this.firstName() + " " + this.lastName();
+        }, this);
+    }
 
-   return homeContentViewModel;
+    return homeContentViewModel;
 });
